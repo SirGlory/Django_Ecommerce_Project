6e8a4a36af41045bab2d56c04d4915543039afab
SirGlory <gregch4@gmail.com>
2021-06-07 20:59:31 +0200

    Final Commit - Pre Deployment

--- greatkart/settings.py
+++ greatkart/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG',default=True, cast=bool)
 
-ALLOWED_HOSTS = ['snazz-project-env.eba-qsnybuck.us-west-2.elasticbeanstalk.com']
+ALLOWED_HOSTS = ['snazz-project-env.eba-qmequtrv.us-west-2.elasticbeanstalk.com']
 
 
 # Application definition
