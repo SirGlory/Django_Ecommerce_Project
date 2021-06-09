81e5510fce5630e8b559167f0fe91e683229dbd1
SirGlory <gregch4@gmail.com>
2021-06-09 19:52:27 +0200

    fixed reviews error

--- greatkart/views.py
+++ greatkart/views.py
@@ -6,6 +6,7 @@ def home(request):
     products = Product.objects.all().filter(is_available=True).order_by('created_date') # -created_date for reverse order
 
     # Get the reviews
+    reviews = None
     for product in products:
         reviews = ReviewRating.objects.filter(product_id=product.id, status=True)
 
