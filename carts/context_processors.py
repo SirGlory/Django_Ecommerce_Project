6b61c16fe862bc9e0175a4604680c5eafffb64f1
SirGlory <gregch4@gmail.com>
2021-06-09 20:10:02 +0200

    fixed cart error

--- carts/context_processors.py
+++ carts/context_processors.py
@@ -5,7 +5,7 @@ from .views import _cart_id
 def counter(request):
     cart_count = 0
     if 'admin' in request.path:
-        return{}
+        return {}
     else:
         try:
             cart = Cart.objects.filter(cart_id=_cart_id(request))
@@ -14,7 +14,7 @@ def counter(request):
             else:
                 cart_items = CartItem.objects.all().filter(cart=cart[:1])
             for cart_item in cart_items:
-                cart_count +=  cart_item.quantity
+                cart_count += cart_item.quantity
         except Cart.DoesNotExist:
             cart_count = 0
     return dict(cart_count=cart_count)
